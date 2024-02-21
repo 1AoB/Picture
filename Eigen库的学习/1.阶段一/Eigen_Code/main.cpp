5f7b85baffe127704a0f8ee03776a8438cddbb3c
1AoB <78208268+1AoB@users.noreply.github.com>
2024-02-21 12:26:36 +0800

    Update main.cpp

--- Eigen库的学习/1.阶段一/Eigen_Code/main.cpp
+++ Eigen库的学习/1.阶段一/Eigen_Code/main.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+	cout << "<======================>" << endl;
+	Matrix <float, 3, 3> matrixA;
+	matrixA.setZero();
+	cout << matrixA << endl;
+
+	Matrix3f matrixA1;
+	matrixA1.setZero();
+	cout <<'\n' << matrixA1 << endl;
+
+	cout << "<======================>" << endl;
+
+	return 0;
+}
